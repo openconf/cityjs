b909645c7a18f40ad94f1b7206f2ec24a1972382
Vasso <gtadion@gmail.com>
2013-03-19 20:25:26 +0200

    Revert "Update boilerplate with livereload"
    This reverts commit 92670346bccda81939b3a52e55e3b1137cb41bfa.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,5 @@
-var path = require('path');
-var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
-
-var folderMount = function folderMount(connect, point) {
-	return connect.static(path.resolve(point));
-};
-
-var folderDir = function folderDir(connect, point){
-	return connect.directory(path.resolve(point));
-}
-
 module.exports = function(grunt) {
 	grunt.initConfig({
-		connect: {
-			server: {
-				options: {
-					port: 8080,
-					base: './app',
-					hostname: null,
-					// livereload awesomeness
-					middleware: function(connect, options){
-						return [
-						lrSnippet, 
-						folderMount(connect, './app'), 
-						folderDir(connect, './app')];
-					}
-				}
-			}
-		},
-		
 		coffee: {
 			options: {
 				bare: true
@@ -68,29 +40,16 @@ module.exports = function(grunt) {
 			}
 		},
 
-		regarde: {
-			coffee: { 
-				files: [
-					'app/*.coffee',
-					'app/**/*.coffee',
-					'tests/**/*.coffee',
-					'generators/**/*.coffee',
-					'server/*.coffee'
-				],
-				tasks: ['coffee', 'livereload']
-			},
-			scss: {
-				files: [
-					'app/assets/scss/*.scss'
-				],
-				tasks: ['sass', 'livereload']
-			},
-			html: {
-				files: [
-					'app/**/*.html'
-				],
-				tasks: ['livereload']
-			}
+		watch: {
+			files: [
+				'app/assets/scss/*.scss',
+				'app/*.coffee',
+				'app/**/*.coffee',
+				'tests/**/*.coffee',
+				'generators/**/*.coffee',
+				'server/*.coffee'
+			],
+			tasks: ['sass', 'coffee', 'server']
 		},
 
 		testacular: {
@@ -99,17 +58,25 @@ module.exports = function(grunt) {
 					configFile: 'tests/testacular-config.js'
 				}
 			}
+		},
+		server: {
+			base: {
+				port:8080,
+				host: "localhost",
+				staticFolder:"./app"
+			}
 		}
 
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-regarde');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-coffee');
 	grunt.loadNpmTasks('grunt-testacular');
-	grunt.loadNpmTasks('grunt-contrib-livereload');
+	grunt.registerMultiTask('server', 'run Server', function(){
+		require("./server/server")(this.data, grunt);
+	});
 
 	// Setip tasks, wanch should be last
-	grunt.registerTask('run', [ 'coffee', 'connect', 'livereload-start', 'testacular', 'regarde']);
+	grunt.registerTask('run', [ 'coffee', 'server', 'testacular', 'watch']);
 	grunt.registerTask('default', ['run']);
 };
